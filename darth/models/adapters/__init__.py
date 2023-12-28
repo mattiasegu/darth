fe69d07f4a4bb0abc731614a055dffb4664b7a66
mattiasegu <mattia.segu@gmail.com>
2023-12-28 09:47:37 +0100

    Remove outdated dependencies

--- darth/models/adapters/__init__.py
+++ darth/models/adapters/__init__.py
@@ -1,15 +1,9 @@
 from .kd_qdtrack import KDQDTrack
-from .vader_qdtrack import VADERQDTrack
 from .darth_qdtrack import DARTHQDTrack
-from .pl_qdtrack import PLQDTrack
-from .tent_qdtrack import TentQDTrack
 from .base import *  # noqa: F401,F403
 
 
 __all__ = [
     'KDQDTrack',
-    'VADERQDTrack',
     'DARTHQDTrack',
-    'PLQDTrack',
-    'TentQDTrack',
 ]
